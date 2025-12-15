3f2d64298a30b0bac7d1fba95372824eb2d2c8d4
RobinJehn <robin.jehn@gmx.de>
2025-12-15 22:18:03 +0100

    operator binding

--- main.rs
+++ main.rs
@@ -1,3 +1,10 @@
 fn main() {
     println!("Hello world!");
+
+    let remainder = -5 % 3;
+
+    let modu = -5_i32.rem_euclid(3);
+    let modu_with_brackets = (-5_i32).rem_euclid(3);
+
+    println!("{remainder} {modu} {modu_with_brackets}");
 }
